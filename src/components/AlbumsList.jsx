429d40f4f3012e2f45aae3fdc90dc2be0f1b6971
Jinnie <hjk7639@gmail.com>
2024-08-29 10:08:37 +0900

    A Few Immediate Notes

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,4 +1,12 @@
+import { useFetchAlbumsQuery } from '../store/apis/albumsApi';
+
 function AlbumsList({ user }) {
+    const results = useFetchAlbumsQuery(user);
+
+    console.log(results);
+
+    // console.log(data, error, isLoading);
+
     return <div>Albums For {user.name}</div>;
 }
 
