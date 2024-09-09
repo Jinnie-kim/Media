25acb337516e35b9916cba2db22737362d9508a9
Jinnie <hjk7639@gmail.com>
2024-09-09 20:30:35 +0900

    Styling Fixups

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -31,9 +31,11 @@ function AlbumsList({ user }) {
 
     return (
         <div>
-            <div>
-                Albums For {user.name}
-                <Button onClick={handleAddAlbum}>+ Add Album</Button>
+            <div className="m-2 flex flex-row items-center justify-between">
+                <h3 className="text-lg font-bold">Albums For {user.name}</h3>
+                <Button loading={results.isLoading} onClick={handleAddAlbum}>
+                    + Add Album
+                </Button>
             </div>
             <div>{content}</div>
         </div>
